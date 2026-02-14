c5c4784e132bcf1548f85d94878bda7c8ae9e9fe
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-02-14 15:06:54 -0700

    yuhh

--- Assets/_Project/Scripts/Battle/Actions/NestedAssets/NestedAsset.cs
+++ Assets/_Project/Scripts/Battle/Actions/NestedAssets/NestedAsset.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 
-
     public abstract class NestedAsset : ScriptableObject
     {
         public abstract string GetNestedAssetName();
-
     }
---
+++ Assets/_Project/Scripts/Battle/Actions/NestedAssets/TestScriptableObject.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Create TestScriptableObject", fileName = "TestScriptableObject", order = 0)]
+public class TestScriptableObject : ScriptableObject
+{
+}
